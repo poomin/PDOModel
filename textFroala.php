c5c7ea5e3e2b37e9b1e3fc1a07b41de82660ff08
x3develop <x3develop@gmail.com>
2018-06-18 15:08:47 +0700

    cherry

--- textFroala.php
+++ textFroala.php
@@ -53,7 +53,6 @@
 
 <div id="editor">
     <div id='edit' style="margin-top: 30px;">
-
     </div>
 </div>
 
---
+++ textTable.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EPOP
+ * Date: 6/18/2018
+ * Time: 2:58 PM
+ */
